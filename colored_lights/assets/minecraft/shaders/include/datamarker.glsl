7d002b45157d2279108cf5ee9fa60f0ba397723f
jnngl <jnngles@gmail.com>
2024-11-27 18:08:59 +1100

    Fix

--- colored_lights/assets/minecraft/shaders/include/datamarker.glsl
+++ colored_lights/assets/minecraft/shaders/include/datamarker.glsl
@@ -21,7 +21,7 @@ vec2 markerSize() {
 }
 
 bool discardDataMarker(ivec2 pixel) {
-    return pixel.y >= 1.0 || pixel.x >= 39.0;
+    return pixel.y >= 1 || pixel.x >= 39;
 }
 
 vec4 writeDataMarker(ivec2 pixel, mat4 projMat, float fogStart, float fogEnd, vec3 chunkOffset, float gameTime, mat3 viewMat, vec2 projUnjitter) {
